7b4decf4b1cee2f984b575c9d0fbc4d77132e80c
k.smolecki <k.smolecki@wasko.pl>
2023-01-05 19:50:42 +0100

    Add and edit note in new view

--- Notatnik/Shared/NoteDto.cs
+++ Notatnik/Shared/NoteDto.cs
@@ -10,11 +10,14 @@ namespace Notatnik.Shared
 {
     public class NoteDto
     {
-        [Required, MinLength(6)]
+        [Required, MinLength(3), MaxLength(22)]
+        [RegularExpression(@"^[a-zA-Z0-9_.]*$", ErrorMessage = "Allowed only leters, numbers and _ .")]
         public string Title { get; set; } = string.Empty;
         public string Body { get; set; } = string.Empty;
         public bool Secure { get; set; } = true;
         public bool Public { get; set; } = false;
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9]*$", ErrorMessage = "Allowed only leters, numbers and")]
         public string Password { get; set; } = string.Empty;
 
     }
